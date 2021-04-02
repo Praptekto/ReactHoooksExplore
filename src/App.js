c6b47f6cf23d26964bffa0adb8046157b15173f4
PraptektoGIT <praptekto@gmail.com>
2021-04-02 15:10:28 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import useLocalstorage from "./useLocalstorage";
 
 
 
 function App() {
+  const key='name';
+  const [name, setName]=useLocalstorage(key, '')
+  function handlesubmit(e){
+    e.preventDefault();
+    localStorage.setItem(key, JSON.stringify(name))
+
+    setName('')
+  }
   return (
-    <div className="App">
+   <form onSubmit={handlesubmit}>
+      <input 
+      type="text"
+      value={name} 
+      onChange={(e)=>{
+        setName(e.target.value)
+      }}/>
+   </form>
       
-    </div>
+   
   );
 }
 
